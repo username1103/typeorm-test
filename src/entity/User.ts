b7fea4a63f54e5c2ca725a277a425aac6ab1dad1
myeongil <username1103@naver.com>
2022-03-20 01:50:29 +0000

    init: typeorm-test setting

---
+++ src/entity/User.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  age: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
---
+++ test/user.test.ts
@@ -0,0 +1,10 @@
+describe("Init test", () => {
+  test("This is test", async () => {
+    // given
+    expect(1 + 1).toBe(2);
+
+    // when
+
+    // then
+  });
+});
